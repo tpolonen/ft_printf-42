9dccfd48c06f6667c3580fd71d0a95750526a3f4
tpolonen <merirosvo@kyy.ninja>
2022-05-31 17:53:23 +0300

    minimal progress

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,16 +6,18 @@
 /*   By: tpolonen <tpolonen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/23 11:04:00 by tpolonen          #+#    #+#             */
-/*   Updated: 2022/05/24 17:27:39 by tpolonen         ###   ########.fr       */
+/*   Updated: 2022/05/31 17:53:04 by teppo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdarg.h>
 #include "ft_printf.h"
 
-# Prototype of format tags is:
-# %[flags][width][.precision][length]specifier
+// Prototype of format tags is:
+// %[flags][width][.precision][length]specifier
 
 static const char	flags[] = "-+ #0";
+
 static const char	*length[] = {
         "hh",
         "h",
@@ -30,6 +32,7 @@ static const char   conv[] = "cdieEfgGosuxXpn%";
 
 int	ft_printf(const char *restrict format, ...)
 {
+	va_list	arg;
 	int     n;
     t_dstr	*out;
 
