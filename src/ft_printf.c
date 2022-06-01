9f9fd9eba845e98c6a4ea7c459b7428c5175676a
tpolonen <tpolonen@student.hive.fi>
2022-06-01 12:33:02 +0300

    some planning

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: tpolonen <tpolonen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/23 11:04:00 by tpolonen          #+#    #+#             */
-/*   Updated: 2022/05/31 20:08:24 by tpolonen         ###   ########.fr       */
+/*   Updated: 2022/06/01 12:32:48 by tpolonen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,35 @@
 // csp
 // diouxX with flags hh, h, l, ll
 // f with flags l, L
+
+// so types we actually need to handle are
+// diouxX - integer as either signed/unsigned decimal, unsigned octal or unsigned hex
+// c - first byte of argument is printed, so char
+// s - string, so char pointer until null byte is reached
+// p - is pointer address if I remember correctly, so also integer? in hex
+// f - float, infinity and NaN are printed as "inf" and "nan" respectively
+
+// so types are
+// - integer
+// - char (pointer)
+// - float
+
+// not too bad to handle with if/else checks. we only need to get flag and type
+// passed to function that handles the actual conversion
+// so we have functions
+// * handle_int(va_arg, flags, seek)
+//  - branches off to handle_char, handle short, handle_long, handle_long_long
+//  - or alternatively convert the input always to long long...
+//  - we probably need to branch off to different outputs eventually anyways,
+//    order of operations matters.
+// * handle_char_ptr(va_arg, flags, seek)
+//  - two cases: print char or print string, probably can be handled in this function
+// * handle_float(va_arg, flags, seek)
+//  - small f = print infinity and nan as `inf`, `nan`
+//  - big F = print infinity and nan as `INF`, `NAN`
+
+// probably special case for %%, no need to go through the whole rigamarole...
+// we already have a special case for %%, no need to make it any more complicated.
 
 // Also needs to be managed:
 // %%
@@ -85,10 +114,11 @@ static int *get_token(const char *start)
 
 int	ft_printf(const char *restrict format, ...)
 {
-//	va_list	arg;
+	va_list	arg_ptr;
 	int		n;
 	t_dstr	*out;
 
+	va_start(arg_ptr, format);
 	n = 0;
 	if (ft_dstrnew(&out, 8) == -1)
 		return (-1);
@@ -102,12 +132,28 @@ int	ft_printf(const char *restrict format, ...)
 			//get token as bits in int
 			//feed token, dstr and corresponding item in va_args (arg itself?) to dispatcher
 			//so something like
-			//convert(get_token(format + n), out, arg);
+			//convert(get_token(format + n), &out, arg);
 			//do we need to squeeze out the next arg here?
 			//how's that gonna work?
+			//after getting full token we know which type the next conversion will be
+			//can we get the next token by size in bytes or do we have to provide type?
+			//I'm thinking of storing types/bitecounts in array where index corresponds
+			//to specific conversion flag, so it would be possible to provide size of
+			//arg after token has been processed
+			//do we need to iterate through the string first to get token count so we know
+			//the size of va_list? linked list would make sense here, since we need to
+			//insert processed tokens between plaintext strings
+			//looks like va_arg(dest, type) is kind of iterator. so no need to count
+			//what va_arg returns if there's not enough args for tokens?
+			//random errors. how can we handle that situation without counting tokens
+			//first and comparing them to args. we can't check for next arg without
+			//calling va_arg.
+			//can we actually expect that va_arg returns -1 after it's emptied?
+			//probably worth testing in cluster & with ft_printf testers.
 			;
 		n++;
 	}
+	va_end(arg_ptr);
 	write(1, out->str, out->len);
 	return (ft_dstrclose(&out, NULL));
 }
